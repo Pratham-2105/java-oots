dbaa5232ee1e3623b7d16b9bb04424bd4a2a4911
Pratham <contact.pratham21@gmail.com>
2025-09-15 10:51:36 +0530

    Updated Day3

---
+++ Day3/defaultDataType.java
@@ -0,0 +1,27 @@
+package Day3;
+
+class DefaultDataTypes1 {
+    static int i;
+    static float f;
+    static double d;
+    static boolean bool;
+    static char c;
+    static short s;
+    static byte b;
+
+    public static void showDefault() {
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bool);
+        System.out.println("[ " + c + " ]");
+        System.out.println(s);
+        System.out.println(b);
+    }
+}
+
+public class defaultDataType {
+    public static void main(String args[]) {
+        DefaultDataTypes1.showDefault();
+    }
+}
--- Day3/demo.java
+++ Day3/demo.java
@@ -1,7 +1,33 @@
 package Day3;
 
+class Inheritance {
+    int counter = 0; // instance variable;
+
+    public void showCounter() {
+        System.out.println(counter);
+    }
+}
+
 public class demo {
     public static void main(String args[]) {
-            String hello = "This is something random!!!";
+        Inheritance inh = new Inheritance();
+        for (int i = 0; i < 10; i++) {
+            ++inh.counter;
+            inh.showCounter();
+        }
+
+        System.out.println("After Loop: " + inh.counter);
+
+        Inheritance inh1 = new Inheritance();
+
+        for (int i = 0; i < 6; i++) {
+            ++inh1.counter;
+            inh1.showCounter();
+        }
+        System.out.println("New Object Counter: " + inh1.counter);
+
+        Inheritance inh2 = new Inheritance();
+        System.out.print("New Counter: ");
+        inh2.showCounter();
     }
 }
